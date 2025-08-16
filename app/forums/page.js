c5d996d8c29ddb5ed7d54ef8567c6c5f85fb36b5
Shriyans <shriyans696@gmail.com>
2025-08-16 22:36:00 +0530

    on going streak

--- app/forums/page.js
+++ app/forums/page.js
@@ -4,32 +4,32 @@ import Image from 'next/image'
 const topics = [
     {
         text: "Python",
-        img: "/python.png",
+        img: "/python.webp",
         desc: "Discuss about Python programming language"
     },
     {
         text: "JavaScript",
-        img: "/javascript.png",
+        img: "/javascript.webp",
         desc: "Explore the fundamentals of JavaScript"
     },
     {
         text: "HTML",
-        img: "/html.png",
+        img: "/html.webp",
         desc: "A deep dive into Hypertext Markup Language"
     },
     {
         text: "CSS",
-        img: "/css.png",
+        img: "/css.webp",
         desc: "Styling web pages with Cascading Style Sheets"
     },
     {
         text: "React",
-        img: "/react.png",
+        img: "/react.webp",
         desc: "Building user interfaces with the React library"
     },
     {
         text: "Node.js",
-        img: "/nodejs.png",
+        img: "/nodejs.webp",
         desc: "Server-side programming with Node.js"
     }
 ];
@@ -37,14 +37,14 @@ const topics = [
 const Forums = () => {
     return (
         <div className='container mx-auto my-28'>
-            <h1 className='text-4xl text-center font-bold'>Discussion Forums</h1>
+            <h3 className='text-5xl text-center font-semibold flex gap-2 justify-center mb-10'>Discussion Forums</h3>
             <div className='flex flex-wrap justify-center'>
                 {topics.map((topic) => {
-                    return <div key={topic.img} className='shadow-lg bg-slate-500 w-1/4 m-4 flex justify-center flex-col items-center py-10'>
-                        <Image src={topic.img} width={34} height={34} />
+                    return <div key={topic.img} className='shadow-lg bg-slate-200 w-1/4 m-4 flex justify-center flex-col items-center py-10 rounded-xl gap-4'>
+                        <Image alt={topic.desc} src={topic.img} width={74} height={74} className=''/>
                         <h2 className='text-2xl'>{topic.text}</h2>
-                        <p>{topic.desc}</p>
-                        <button>
+                        <p className='px-3'>{topic.desc}</p>
+                        <button className='px-4 border border-slate-300 py-2'>
                             Discuss Now
                         </button>
                     </div>
