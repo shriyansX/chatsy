76d4746259e2d4c8b8bc35479410bfe87503aa33
Shriyans <shriyans696@gmail.com>
2025-08-17 12:50:37 +0530

    almost done

--- app/forum/[slug]/page.js
+++ app/forum/[slug]/page.js
@@ -1,7 +1,9 @@
 import ChatForum from '@/components/ChatForum';
+import { currentUser } from '@clerk/nextjs/server'
 
 
 export default async function Page({ params }) {
+   const user = await currentUser()
   const { slug } = await params
-  return <ChatForum slug={slug} />
-};
+  return <ChatForum slug={slug} clerkUser={{ id: user.id, name: user.firstName, token: user.publicMetadata.token }} />
+}
--- components/ChatForum.js
+++ components/ChatForum.js
@@ -1,30 +1,23 @@
 "use client"
+import React from 'react'
 import { useState, useEffect } from 'react';
 import { useCreateChatClient, Chat, Channel, ChannelHeader, MessageInput, MessageList, Thread, Window } from 'stream-chat-react';
-import { useUser } from '@clerk/nextjs';
 
 import 'stream-chat-react/dist/css/v2/index.css';
 
-const apiKey = 'pq7a699xvrx2';
-// const userId = 'user_31NTHQjLKDbLmOI5k9KKDVrLcQy';
-// const userName = 'Shriyans';
-const userToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoidXNlcl8zMU5USFFqTEtEYkxtT0k1azlLS0RWckxjUXkifQ.VG2upuVTnV4o2tSHlmpd7tNXRp4MXny5PXDYnzeb86w';
 
+const ChatForum = ({ clerkUser, slug }) => {
+    const apiKey = 'pq7a699xvrx2';
+    const userId = clerkUser.id;
+    const userName = clerkUser.name;
+    const userToken = clerkUser.token;
 
-export default function ChatForum({slug}) {
-  const clerkUser = useUser();
-  const user = {
-    id: clerkUser.user.id,
-    name: clerkUser.user.firstName,
-    image: `https://getstream.io/random_png/?name=${clerkUser.user.name}`,
-  };
-  function toTitleCase(str) {
-    return str.replace(
-      /\b[a-z]/g,
-      (char) => char.toUpperCase()
-    );
-  }
-  const [channel, setChannel] = useState();
+    const user = {
+  id: userId,
+  name: userName,
+  image: `https://getstream.io/random_png/?name=${userName}`,
+};
+    const [channel, setChannel] = useState();
   const client = useCreateChatClient({
     apiKey,
     tokenOrProvider: userToken,
@@ -36,7 +29,7 @@ export default function ChatForum({slug}) {
 
     const channel = client.channel('messaging', slug, {
       image: 'https://getstream.io/random_png/?name=react',
-      name: toTitleCase(slug.replace(/-/g, " ")) + ' Discussion',
+      name: slug.toUpperCase() + "Discussion",
       members: [userId],
     });
 
@@ -57,4 +50,6 @@ export default function ChatForum({slug}) {
       </Channel>
     </Chat>
   );
-};
+}
+
+export default ChatForum
